1ec11a7c07ac9f65ff40acd46808103215ac747c
JorgeNunes <jlbununes@live.com>
2021-02-12 18:30:00 -0300

    Melhorando front

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,8 +40,8 @@ function initScrollSuave(){
     const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
     function scrollToSection(event){
         event.preventDefault();
-        const href = this.getAttribute('href');
-        const section = document.querySelector(href);
+        const hrefs = this.getAttribute('href');
+        const section = document.querySelector(hrefs);
         section.scrollIntoView({
             behavior: 'smooth',
             block: 'start', 
